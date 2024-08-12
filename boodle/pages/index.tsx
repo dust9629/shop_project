bc416e03b9d077e5963db6c2fbfe1f62887ea157
EunJi Kim <dust9629@gmail.com>
2024-08-12 23:59:06 +0900

    test : add test code

--- boodle/pages/index.tsx
+++ boodle/pages/index.tsx
@@ -1,6 +1,19 @@
 // pages/index.tsx
+import React, { useState } from "react";
+
 function HomePage() {
-  return <div>Welcome to My Next.js App!</div>;
+  const [message, setMessage] = useState("Welcome to My Next.js App!");
+
+  function handleClick() {
+    setMessage("Button clicked! The message has changed.");
+  }
+
+  return (
+    <div>
+      <h1>{message}</h1>
+      <button onClick={handleClick}>Click me to change the message!</button>
+    </div>
+  );
 }
 
 export default HomePage;
